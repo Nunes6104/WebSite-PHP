4f49e5f061b1f5851b4da1e295ca094cd2788a38
Nunes6104 <jmmnunes6104@gmail.com>
2024-11-19 08:43:38 +0000

    Nunes67104

---
+++ dobro.php
@@ -0,0 +1,15 @@
+<?php 
+
+const escola = "ISTEC";
+define ("nl", "<br/>");
+$idade = 22.8;
+$nome = 'Jose';
+$ok = true;
+$rslt = "$nome tem $idade";
+print ($idade);
+print $nome;
+
+echo $rslt;
+echo $rslt . " - " . $ok;
+$rslt2 = "$nome tem $idade";
+?>
